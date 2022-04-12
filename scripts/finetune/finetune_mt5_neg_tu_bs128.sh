100114359cf47122a76a95a0f9d795ec3d7d6164
Isabella Senturia <ies9@grace1.grace.hpc.yale.internal>
2022-04-12 18:53:28 -0400

    added Turkish datasets and script

---
+++ scripts/finetune/finetune_mt5_neg_tu_bs128.sh
@@ -0,0 +1,32 @@
+
+
+#!/bin/bash
+
+#SBATCH --job-name=MT5-base-finetune-neg-tu
+#SBATCH --output=joblogs/test_neg_tu_%j.txt
+#SBATCH --nodes=1 
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30GB 
+#SBATCH --time=07:00:00
+#SBATCH --gpus=v100:1
+#SBATCH --partition=gpu
+
+module load miniconda
+
+source activate py38
+
+cd ~/project/multilingual_transformations
+
+python models/run_seq2seq.py \
+    --model_name_or_path 'google/mt5-base' \
+    --do_train \
+    --task translation_src_to_tgt \
+    --train_file data/neg_tu/neg_tu-no_indef_train.json.gz \
+    --validation_file data/neg_tu/neg_tu-no_indef_dev.json.gz \
+    --output_dir outputs/mt5-finetuning-neg-tu-no-indef-bs128/  \
+    --per_device_train_batch_size=8 \
+    --gradient_accumulation_steps=16 \
+    --per_device_eval_batch_size=16 \
+    --overwrite_output_dir \
+    --predict_with_generate \
+    --num_train_epochs 10.0
